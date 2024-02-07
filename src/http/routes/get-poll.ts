4c4aa99208aead793849016067702c3cdcc1b02b
Vicente <vikom.sanchez@gmail.com>
2024-02-07 19:18:33 -0300

    feat: add options score when get poll

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,4 +1,5 @@
 import { db } from '@/lib/prisma'
+import { redis } from '@/lib/redis'
 import { FastifyInstance } from 'fastify'
 
 import z from 'zod'
@@ -26,10 +27,37 @@ export async function GetPoll(app: FastifyInstance) {
     })
 
     if (!poll)
-      res
-        .status(404)
+      return res
+        .status(400)
         .send({ success: false, code: 404, message: 'Poll not found' })
 
-    return res.status(200).send({ success: true, code: 200, poll })
+    const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+    const votes = result.reduce(
+      (obj, line, i) => {
+        if (i % 2 === 0) {
+          const score = result[i + 1]
+
+          Object.assign(obj, { [line]: Number(score) })
+        }
+
+        return obj
+      },
+      {} as Record<string, number>,
+    )
+
+    return res.status(200).send({
+      success: true,
+      code: 200,
+      poll: {
+        ...poll,
+        options: poll.options.map((option) => {
+          return {
+            id: option.id,
+            title: option.title,
+            score: option.id in votes ? votes[option.id] : 0,
+          }
+        }),
+      },
+    })
   })
 }
